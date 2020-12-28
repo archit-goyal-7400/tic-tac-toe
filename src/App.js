73c141a9923192462e597094666b491e643c456c
archit828 <garchit2000@gmail.com>
2020-12-28 23:22:22 +0530

    moving state up

--- src/App.js
+++ src/App.js
@@ -2,15 +2,84 @@ import React from "react";
 import Board from "./Board";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        },
+      ],
+      isNextX: true,
+    };
+  }
+  stateHandler = (i) => {
+    if (
+      !this.state.history[this.state.history.length - 1].squares[i] &&
+      !this.winnerHandler(
+        this.state.history[this.state.history.length - 1].squares
+      )
+    ) {
+      const history = [...this.state.history];
+      const square = this.state.history[
+        this.state.history.length - 1
+      ].squares.slice();
+      square[i] = this.state.isNextX ? "X" : "O";
+      history.push({ squares: square });
+      this.setState({ history: history, isNextX: !this.state.isNextX });
+    }
+  };
+  winnerHandler = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
   render() {
+    console.log(this.state.history);
+    const winner = this.winnerHandler(
+      this.state.history[this.state.history.length - 1].squares
+    );
+    let status = "Next player: " + (this.state.isNextX ? "X" : "O");
+    if (winner) {
+      status = "Winner is " + winner;
+    }
+    const moves = this.state.history.map((sq, i) => {
+      return (
+        <li key={i}>
+          <button>{i ? "Move to #" + i : "Move to start"}</button>
+        </li>
+      );
+    });
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={this.state.history[this.state.history.length - 1].squares}
+            isNextX={this.state.isNextX}
+            stateHandler={(i) => this.stateHandler(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
--- src/Board.js
+++ src/Board.js
@@ -2,67 +2,19 @@ import React from "react";
 import Square from "./Square";
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      isNextX: true,
-      winnerFinal: false,
-    };
-  }
-  stateHandler = (i) => {
-    if (!this.state.squares[i] && !this.state.winnerFinal) {
-      const squares = this.state.squares.slice();
-      squares[i] = this.state.isNextX ? "X" : "O";
-      this.setState({ squares: squares, isNextX: !this.state.isNextX });
-    }
-  };
-  winnerHandler = () => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        this.state.squares[a] &&
-        this.state.squares[a] === this.state.squares[b] &&
-        this.state.squares[a] === this.state.squares[c]
-      ) {
-        return this.state.squares[a];
-      }
-    }
-    return null;
-  };
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.stateHandler(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.stateHandler(i)}
       />
     );
   }
 
   render() {
-    const winner = this.winnerHandler();
-    if (winner && !this.state.winnerFinal) {
-      this.setState({
-        winnerFinal: true,
-      });
-    }
-    let status = "Next player: " + (this.state.isNextX ? "X" : "O");
-    if (winner) {
-      status = "Winner is " + winner;
-    }
+    console.log(this.props.squares);
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
